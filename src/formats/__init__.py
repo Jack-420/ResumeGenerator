2e67083063b48211513f1a6809d40fba3f389c9f
Amulya Paritosh <amulya1171@gmail.com>
2024-01-20 18:31:03 +0530

    cli arguments added

--- src/formats/__init__.py
+++ src/formats/__init__.py
@@ -4,4 +4,4 @@ from .single_column_photo import create_document as single_column_photo_resume
 
 
 class ResumeFormat(Enum):
-    SINGLE_COLUMN_PHOTO = 0
+    SINGLE_COLUMN_PHOTO = "single_column_photo"
--- src/resume_generator.py
+++ src/resume_generator.py
@@ -7,9 +7,8 @@ from .formats import ResumeFormat, single_column_photo_resume
 def create_resume(data_path: Path, output_path: Path, resume_format: ResumeFormat):
     data_obj = load_data(data_path)
 
-    match resume_format:
-        case ResumeFormat.SINGLE_COLUMN_PHOTO:
-            latex_resume = single_column_photo_resume(data_obj)
+    if resume_format is ResumeFormat.SINGLE_COLUMN_PHOTO:
+        latex_resume = single_column_photo_resume(data_obj)
 
     latex_resume.generate_pdf(str(output_path))
     latex_resume.generate_tex(str(output_path))
