c182590b448c23927a7ba1aafdfd3b453f5436a2
A.Paritosh <amulya1171@gmail.com>
2024-06-05 15:17:23 +0530

    feat: Update resume router to use ResumeTemplate value in read_template_metadata function

--- ResumeGenerator/src/resume/router.py
+++ ResumeGenerator/src/resume/router.py
@@ -29,7 +29,7 @@ async def read_template_metadata(
     template_name: ResumeTemplateEnum,
 ) -> ResumeTemplateMetadata:
     try:
-        return ResumeTemplate.get_template_metadata(template_name)
+        return ResumeTemplate.get_template_metadata(template_name.value)
     except ValueError as err:
         raise HTTPException(status_code=404, detail=str(err)) from err
 
